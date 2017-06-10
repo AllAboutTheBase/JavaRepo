e50bd0ba0633e662efb845f0756871f486a2ecb0
grygiel <GeorgeRygiel@comcast.net>
2017-06-10 16:15:12 -0400

    this a new feature

--- TestGitProject/src/Testpackage/GRMainClass.java
+++ TestGitProject/src/Testpackage/GRMainClass.java
@@ -3,7 +3,7 @@ package Testpackage;
 public class GRMainClass {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD FIX");
+		System.out.println("New Feature");
 		System.out.println("hello world");////
 	System.out.println("hello world Test 1 changed in displyay data");
 //
