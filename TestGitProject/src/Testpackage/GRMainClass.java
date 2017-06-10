f7da173711b71f59b4b839c0fa79f0c651e00bf5
grygiel <GeorgeRygiel@comcast.net>
2017-06-10 15:32:18 -0400

    another chang on original

--- TestGitProject/src/Testpackage/GRMainClass.java
+++ TestGitProject/src/Testpackage/GRMainClass.java
@@ -3,7 +3,7 @@ package Testpackage;
 public class GRMainClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world");//
 	System.out.println("hello world Test 1 changed in displyay data");
 //
 	}
