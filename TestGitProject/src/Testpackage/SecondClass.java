83f87955bd8b72fcbeb4453265d8c9149f53609e
grygiel <GeorgeRygiel@comcast.net>
2017-06-10 16:18:56 -0400

    made few changes in feature1

--- TestGitProject/src/Testpackage/SecondClass.java
+++ TestGitProject/src/Testpackage/SecondClass.java
@@ -5,5 +5,7 @@ int var;
 	public void test()
 	{
 	System.out.println("hello world display--- data branch");
+	System.out.println("This one was updated in feature 1hello world display--- data branch");
+	
 	}
 }
