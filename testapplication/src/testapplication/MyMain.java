3db11ac7531f9c157203f4921563f910acee5ccf
grygiel <GeorgeRygiel@comcast.net>
2017-06-11 10:29:30 -0400

    adde ref to master class

---
+++ testapplication/src/testapplication/MyMain.java
@@ -0,0 +1,14 @@
+package testapplication;
+
+import base.InSameRepo;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		frameworkclass obj = new frameworkclass();
+		InSameRepo obj2 = new InSameRepo();
+		
+		
+	}
+
+}
--- testapplication/src/testapplication/app1class.java
+++ testapplication/src/testapplication/app1class.java
@@ -1,5 +1,5 @@
 package testapplication;
 
 public class app1class {
-
+// Changed application at same time
 }
