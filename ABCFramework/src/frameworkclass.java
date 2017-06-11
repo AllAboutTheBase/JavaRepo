4de64bc8575592738a65fdb034805843039d7421
grygiel <GeorgeRygiel@comcast.net>
2017-06-11 10:27:11 -0400

    comit method

--- ABCFramework/src/frameworkclass.java
+++ ABCFramework/src/frameworkclass.java
@@ -3,5 +3,8 @@ public class frameworkclass {
 //  Changed framework
   // added on web want to see refresh
   // Someone made change before I pushed
+	public void mMyMethod(){
+		System.out.println("hello");
+	}
   
 }
